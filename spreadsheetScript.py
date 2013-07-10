d6a3f7512d18dbeb58e25c3331e4f1c66db04f12
Nanette Taylor <nanette.m.taylor@gmail.com>
2013-07-10 15:31:53 +0000

    Formatted print output

--- spreadsheetScript.py
+++ spreadsheetScript.py
@@ -54,14 +54,14 @@ class SpreadsheetScript():
 		# print field titles of data
 		for row in feed.entry:
 			for key in row.custom:
-				print key,
+				print key+'\t\t',
 			print
 			break
 		
 		# print records in each row
 		for row in feed.entry:
 			for key in row.custom:
-				print "%s" % (row.custom[key].text),
+				print "%s" % (row.custom[key].text)+'\t\t',
 			print
 	
 	# return a list of spreadsheets titles
@@ -221,6 +221,8 @@ Main Options
 		Delete cells, columns or entire rows from the spreadsheet
 	--print
 		Does not take any character after it, if specified will print the contents of the document name provided.
+	--help
+		Prints this screen
 	"""
 	
 def main():
